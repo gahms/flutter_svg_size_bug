9b16e1d68d853e9ac14c0ca095f1ec1665a2ea4c
Nicolai Henriksen <nih@miracle.dk>
2019-02-07 17:36:07 +0100

    .

--- asset_size_demo/lib/main.dart
+++ asset_size_demo/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() => runApp(MyApp());
 
@@ -23,7 +24,16 @@ class MyHomePage extends StatelessWidget {
         title: Text('Size Demo'),
       ),
       body: Center(
-        child: Image.asset('assets/stick_figure.png')
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('As PNG:'),
+            Image.asset('assets/stick_figure.png'),
+            Container(height: 80),
+            Text('As SVG:'),
+            SvgPicture.asset('assets/stick_figure.svg'),
+          ],
+        )
       ),
     );
   }
